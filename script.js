be2818289390523d123ebbab37b5cf32ccda1acb
Aditya7903 <aviartstudio@gmail.com>
2022-01-07 11:43:12 +0530

    tyi

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
 console.log("welcome to Spotify");
-let audioElement =new  Audio('music/1.mp3');
+//Initilize the variables
+let songIndex = 0;
+let audioElement = new Audio("music/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let song = [
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+  {
+    songName: "Salame-a-Ishq",
+    filePath: "music/1.mp3",
+    coverPath: "images/1.jpg",
+  },
+];
+
 //audioElement.play();
+//handle play and pause click
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-play-circle");
+    masterPlay.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+  }
+});
+//Listen to events
+myProgressBar.addEventListener("timeupdate", () => {
+  console.log("timeupdate");
+});
